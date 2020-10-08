606e0b7f0ec0d7dc21422585dc998e1e9ccacebd
vivthai <57568661+vivthai@users.noreply.github.com>
2020-10-08 18:40:09 -0400

    1

--- Lab_4/js/scripts.js
+++ Lab_4/js/scripts.js
@@ -1,4 +1,4 @@
-let myFullName = "Bill Farmer"; // 10 characters in name...not including space
+let myFullName = "Vivian Thai"; // 10 characters in name...not including space
 // 1 Change the content of the div with class "header" to "[Your name]'s Lab 4"
 const myHeading = document.querySelector('.header');
 myHeading.textContent = "Vivian Thai's Lab 4";
@@ -12,19 +12,25 @@ secondParagraph.className = 'content2';
 
 document.querySelector('.content').append(firstParagraph, secondParagraph);
 
-
 // 3 Into the first paragraph, insert the phrase "my name has " length of your name " characters"
 //      (e.g. my name has 10 characters).
-
+firstParagraph.innerText = "my name has " + (myFullName.length - 1) + " characters.";
 
 // 4 & 5 Into the second paragraph tag, return the 3rd character in your first name
+secondParagraph.innerText = "the third character in my name is " + myFullName.slice(2,3).toUpperCase()
 
 // 6 Add a new line to your second paragraph
+secondParagraph.innerText += "\n";
 
 // 7 Return the final three characters of your last name to that new line
+secondParagraph.innerText += myFullName.slice(myFullName.toUpperCase.length-3);
 
 // 8 Substring your first and last name into two separate variables
+let firstName = myFullName.slice(0,6);
+let lastName = myFullName.slice(7);
 
 // 9 Add the total length of your first and last names together
+let total = firstName.length + lastName.length;
 
 // 10 Display that total next to your name in your header
+myHeading.innerText += " " + total;
