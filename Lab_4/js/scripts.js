654a1684e6c6e9bd0766cbe53563052009dae5e2
vivthai <57568661+vivthai@users.noreply.github.com>
2020-10-07 22:04:25 -0400

    1

--- Lab_4/js/scripts.js
+++ Lab_4/js/scripts.js
@@ -1,12 +1,21 @@
 let myFullName = "Bill Farmer"; // 10 characters in name...not including space
 // 1 Change the content of the div with class "header" to "[Your name]'s Lab 4"
+const myHeading = document.querySelector('.header');
+myHeading.textContent = "Vivian Thai's Lab 4";
 
 // 2 Insert two paragraphs into the div with the class "content"
 //  Label each paragraph with a distinct class name
+let firstParagraph = document.createElement("p");
+firstParagraph.className = 'contentOne';
+let secondParagraph = document.createElement("p");
+secondParagraph.className = 'content2';
+
+document.querySelector('.content').append(firstParagraph, secondParagraph);
 
 
 // 3 Into the first paragraph, insert the phrase "my name has " length of your name " characters"
 //      (e.g. my name has 10 characters).
+
 
 // 4 & 5 Into the second paragraph tag, return the 3rd character in your first name
 
