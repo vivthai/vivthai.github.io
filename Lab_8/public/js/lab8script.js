5b22ead568506265dcf6fac507e1c417500b4f92
vivthai <57568661+vivthai@users.noreply.github.com>
2020-11-20 20:23:46 -0500

    1

---
+++ Lab_8/public/js/lab8script.js
@@ -0,0 +1,64 @@
+let url = "https://www.reddit.com/r/popular.json";
+let chart = "";
+console.log(url);
+//const fetchPromise = fetch(url);
+
+let settings = { method: "Get" };
+let chartValues = [];
+
+async function getData() {
+    await fetch(url, settings)
+        .then(res => res.json())
+        .then((json) => {
+            let listSize = json.data.children.length; //list size
+            // Loop to pick 5 random entries
+            for (x = 0; x < 5; x++) {
+                num=Math.floor(Math.random() * listSize);
+                theData = json.data.children[number].data;
+                subred= theData.subreddit;
+                auth = theData.author;
+                ti = theData.title;
+                up = theData.ups;
+                let message = "<b>Subreddit </b>: " + subred + " <b>Author</b>:" + ti + " <b>Up votes</b>: " + up;
+                let createli = document.createElement("li"); //put message in li
+                liforappend = createli.innerHTML = message;
+                let theredditlist = document.querySelector("redditList");
+                theredditlist.append(liforappend); //add li to theredditlist
+                /*
+                    Get a random number within the size of the list
+                    Get subreddit, author, title, and ups from record
+                    Set the message to be:
+                        let message = "<b>Subreddit </b>: " + subreddit + " <b>Author</b>:" + author + " <b>Title</b>:" + title + " <b>Up votes</b>: " + ups;
+                        Add a new <li> element with the message to the 'redditList' element
+                    Add a data entry to chartValues with author as the label and ups as the y component
+                */
+                /*.......*/
+                let addToChart = {'label':author,y:ups}; // Gave this. This needs to be added to the 'chartValues'
+                /*.......*/
+                chartValues.push(addToChart);
+            }
+        })
+        .then(values => console.log(chartValues));
+        chart.render(); // Do you need to remove the comments from here in order to get it to work? Yes you do.
+};
+
+window.onload = async function makeChart() {
+    getData();
+    chart = new CanvasJS.Chart("chartContainer", {
+        title: {
+            text: "Upvotes"
+        },
+
+        data: [     
+            { 
+                type: "column",
+                name: "Popular Reddit",
+                dataPoints: // WHAT GOES HERE???
+            }
+        ]
+    });
+    
+    chart.render();
+}
+
+//window.onload = makeChart();
