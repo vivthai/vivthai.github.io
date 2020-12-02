56d6f8f42ea0428cfccc396a33b2bc4250c84620
vivthai <57568661+vivthai@users.noreply.github.com>
2020-12-01 19:44:13 -0500

    Done!

---
+++ Lab_9/src/routes/tasks.js
@@ -0,0 +1,108 @@
+let express = require('express')
+let router = express.Router()
+let db = require('../database');
+
+// Create a new task
+// POST localhost:<port>/task
+router.post('/task', (req, res) => {
+    // now we have access to req.body due to body-parser (see index.js)
+    if (!req.body) {
+        return resizeBy.status(400).send('Request body is missing')
+    }
+
+    console.log(req.body)
+    let task = {
+        name: req.body.taskName,
+        dueDate: req.body.taskDueDate // Example 2020-11-24
+    }
+    
+    var sql ='INSERT INTO tasklist (taskName, dueDate) VALUES (?,?)'
+    var params =[task.name, task.dueDate]
+    db.run(sql, params, function (err, result) {
+        if (err){
+            res.status(400).json({"error": err.message})
+            return;
+        }
+        res.json({
+            "message": "success",
+            "data": task,
+            "id" : this.lastID
+        })
+    });
+    
+})
+
+//GET
+router.get('/task', (req, res) => {
+    console.log(req.query)
+    if (!req.query.taskId) {
+        return res.status(400).send('Missing URL parameter id')
+    }
+    let sql = "select * from tasklist where id = ?"
+    console.log("req.query.taskId: " + req.query.taskId)//TODO)
+    let params = [req.query.taskId]
+    db.get(sql, params, (err, row) => {
+        if (err) {
+          res.status(400).json({"error":err.message});
+          return;
+        }
+        res.json({
+            "message":"success",
+            "data":row
+        })
+      });
+})
+
+//Update
+router.put('/task', (req, res) => {
+    console.log("PUT called")
+    var data = {
+        id : req.query.taskId,
+        taskName: req.body.taskName
+        
+    }
+    console.log("data.id:" + data.id + " name:" + data.taskName)
+    if (!data.id) {
+        return res.status(400).send('Missing URL parameter id')
+    }
+    db.run(
+        `UPDATE tasklist set 
+           taskName = ? 
+           WHERE id = ?`,
+        [data.taskName, data.id],
+        function (err, result) {
+            if (err){
+                res.status(400).json({"error": res.message})
+                return;
+            }
+            res.json({
+                message: "success",
+                data: data,
+                changes: this.changes
+            })
+    });
+})
+
+//Delete
+//TODO add entire DELETE method
+router.delete('/task', (req, res) => {
+    console.log(req.query)
+    if (!req.query.taskId) {
+        return res.status(400).send('Missing URL parameter id')
+    }
+    let sql = "delete from tasklist where id = ?"
+    console.log("req.query.taskId: " + req.query.taskId)//TODO)
+    let params = [req.query.taskId]
+    db.get(sql, params, (err, row) => {
+        if (err) {
+          res.status(400).json({"error":err.message});
+          return;
+        }
+        res.json({
+            "message":"success",
+            "data":row
+        })
+      });
+})
+
+module.exports = router
